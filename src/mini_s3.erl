7f1995db8f177869f313a4a37ccad2cb9a7b3dea
fylfot <mr.fylfot@gmail.com>
2018-12-14 20:15:03 +0200

    Update mini_s3.erl
    Removed envy

--- src/mini_s3.erl
+++ src/mini_s3.erl
@@ -931,15 +931,4 @@ make_authorization(AccessKeyId, SecretKey, Method, ContentMD5, ContentType, Date
     Signature = base64:encode(crypto:hmac(sha, SecretKey, StringToSign)),
     {StringToSign, ["AWS ", AccessKeyId, $:, Signature]}.
 
-default_config() ->
-    Defaults =  envy:get(mini_s3, s3_defaults, list),
-    case proplists:is_defined(key_id, Defaults) andalso
-        proplists:is_defined(secret_access_key, Defaults) of
-        true ->
-            {key_id, Key} = proplists:lookup(key_id, Defaults),
-            {secret_access_key, AccessKey} =
-                proplists:lookup(secret_access_key, Defaults),
-            #config{access_key_id=Key, secret_access_key=AccessKey};
-        false ->
-            throw({error, missing_s3_defaults})
-    end.
+default_config() -> #config{}.
